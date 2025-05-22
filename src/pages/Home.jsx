4f5f7590037a8efbab264649f32cc75a99538c95
MyronyukNestor <116816080+MyronyukNestor@users.noreply.github.com>
2025-05-22 14:29:57 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Hero from "../components/Hero";
 import NewArrivals from "../components/NewArrivals";
 import Features from "../components/Features";
-import PopularBooks from "../components/popularBooks";
+import PopularBooks from "../components/PopularBooks";
 import FeaturesOfSite from "../components/FeaturesOfSite";
 
 const Home = () => {
